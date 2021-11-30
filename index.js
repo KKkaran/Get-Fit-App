13fd1a124aa6660ed39ce0eb5eaab1bb6cd84a8e
Karan Sodhi <karanpreetsodhi1997@gmail.com>
2021-11-30 12:25:45 -0500

    dynamically created drop down muscle groups to tackle the api modification in the future

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var api = "https://getfitapifinal.herokuapp.com/exercises"
-var exerciseObjects = []
-var chosenExercises = []
+var exerciseObjects = []//complete list of objects(exercises) on fetch api call
+var chosenExercises = []//list of objects(exercises) on random on button click
 
 function getExercises(){
 
@@ -16,46 +16,67 @@ function getExercises(){
 
             exerciseObjects.push(obj)
         });
+        console.log(resp)
+        
     })
 }
 
 function displayExercises(){
 
-    var m1 = $("#lang1").find(":selected").text()
-    var m2 = $("#lang2").find(":selected").text()
+    var m1 = $(".muscle1").find(":selected").text()
+    var m2 = $(".muscle2").find(":selected").text()
     var chosenExer = []
     exerciseObjects.forEach((el) =>{
         chosenExer = []
         if((m1 === el["muscle"]) || (m2 === el["muscle"])){
-            //  console.log(el["muscle"])
-            //  console.log(el["exercises"])
-            // console.log(el["muscle"])
             var d = el["exercises"]
-            //console.log("random Now:")
             for(let v=0;v<4;v++){
                 var  g = Math.floor(Math.random() * (d.length - v))
-                //console.log(d[g])
                 chosenExer.push(d[g])
                 d.splice(d.indexOf(d[g]),1) 
             }
-            console.log(chosenExer.length)
             var obj = {
                 "muscle" : el["muscle"],
                 "exercises" : chosenExer
             }
             chosenExercises.push(obj)
         }
-        
     })
-
     console.log(chosenExercises)
 }
+function createMuscleGroups(){
 
-function createRandomIndices(){
-
+    var select1 = $("<select>").addClass("muscle1")
+    select1.append(
+        $("<option>").text("Select Muscle").val(""), 
+    )
+    var select2 = $("<select>").addClass("muscle2")
+    select2.append(
+        $("<option>").text("Select Muscle").val(""), 
+    )
+    var submit = $("<input>").attr({"type":"submit","value":"submit"})
+    $(".formhandler").append(select1,select2,submit)
+    setTimeout(addDynamicMuscleGroups , 1000)
+}
+function addDynamicMuscleGroups(){
+    var select = $(".muscle1")
+    exerciseObjects.forEach(el =>{
+        select.append(
+            $("<option>").text(el["muscle"]).val(el["muscle"]), 
+        )
+    })
+    var select2 = $(".muscle2")
+    exerciseObjects.forEach(el =>{
+        select2.append(
+            $("<option>").text(el["muscle"]).val(el["muscle"]), 
+        )
+    })
+    
 }
 $(".formhandler").on("submit",function(e){
     e.preventDefault()
     displayExercises()
 })
 getExercises() //this will fetch all the exercises
+//createMuscleGroups()//this will create the select options(muscles) in the form
+createMuscleGroups()
