24dcae6db9639f65f531778192ca9493ff50c8a9
Bun Pape <benjaminhpage@me.com>
2021-11-29 21:48:10 -0500

    added clock display and highlightDay function, added ids in index.html

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("working")
+
+// display date and time
+function updateTime() {
+    $('#clock').html(moment().format('MMMM Do YYYY, h:mm:ss a'))
+};
+
+// highlight day on calendar
+var highlightDay = function() {
+    var weekDay = moment().format('dddd').toLowerCase()
+    
+    // check if article shares id with weekday
+    var cardDate = $("#" + weekDay)
+    
+    if (cardDate) {
+        cardDate.css("background-color", "#add8e6")
+    }
+};
+
+highlightDay();
+setInterval(updateTime, 1000);
+updateTime();
