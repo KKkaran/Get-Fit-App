e0b604b0266322428c90d72eeb9bc8b881099806
Karan Sodhi <karanpreetsodhi1997@gmail.com>
2021-11-30 00:12:41 -0500

    fetching from api successfully

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 console.log("working")
+var api = "https://getfitapifinal.herokuapp.com/exercises"
+
+function getExercises(){
+
+    fetch(api)
+    .then(function(res){
+        return res.json()
+    }).then(function(resp){
+        console.log(resp)
+    })
+}
+getExercises()
+
+$(".formhandler").on("submit",function(e){
+    e.preventDefault()
+    console.log("form submitted")
+})
